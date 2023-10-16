d0ce238bc29ec449763f52d4abba5423bc202780
Saigut <gosaigut@gmail.com>
2023-10-16 15:20:52 +0800

    Update 1_run.go

--- Go/base/1_run.go
+++ Go/base/1_run.go
@@ -9,3 +9,11 @@ func main() {
 // * 编译：go build .\1_run.ss.go。再运行
 // * 入口函数 main 必须在 main package 里。
 // * 同一个目录下的文件 package 名字相同。package 名字可以与目录名字相同，也可以不同
+
+// * 例子
+// 结构：
+// proj_dir/
+//   src/
+//     code.go
+// cd proj_dir && go mod init proj_name
+// $ go build ./src/code.go
